9db4e2a6285b9fc62739f2797a9f898ee92b79c2
Magikarp <127387891+not-quality@users.noreply.github.com>
2025-07-03 02:30:06 +0800

    Excel自动化处理

--- excel_auto/excel_auto.py
+++ excel_auto/excel_auto.py
@@ -5,16 +5,27 @@ import os
 os.chdir('./excel_auto')
 # 引入数据
 data = pd.read_excel('./全球票房最高电影TOP15_2025.xlsx')
+# 查看标题行
+print(data.columns.tolist())
+# 方法1：仅获取列名（不读取数据）
+column_names = pd.read_excel('全球票房最高电影TOP15_2025.xlsx', nrows=0)
+print("所有列标题:", column_names)
+# 方法2：读取整个文件后获取列名
+df = pd.read_excel('全球票房最高电影TOP15_2025.xlsx')
+print("所有列标题:", df.columns.tolist())
+# columns：返回所有列名组成的 Index 对象；
+# tolist()：将 Pandas Index 对象转换为 Python 列表；
+
 # 按名称读取
-# df_sheet1 = pd.read_excel('file.xlsx', sheet_name='Sheet1')
+# df_sheet1 = pd.read_excel('全球票房最高电影TOP15_2025.xlsx', sheet_name='Sheet1')
 # 按索引读取（0表示第一个工作表）
-# df_first = pd.read_excel('file.xlsx', sheet_name=0)
+# df_first = pd.read_excel('全球票房最高电影TOP15_2025.xlsx', sheet_name=0)
 # 一次性读取所有工作表
-# all_sheets = pd.read_excel('your_file.xlsx', sheet_name=None)
+# all_sheets = pd.read_excel('全球票房最高电影TOP15_2025.xlsx', sheet_name=None)
 
 # 数据读取
-print(data)
-print(data['类型标签'])
+# print(data)
+# print(data['类型标签'])
 
 # 末尾添加新列，其中apply(lambda x:x.func()) 表示将列表中的每个元素单独提取为x,并对x元素运用func()方法
 data['类型标签-1'] = data['类型标签'].apply(lambda x:x.split('/')[0])
@@ -27,9 +38,14 @@ print(type_list)
 # 注：其中unique()和set()都用于去重
 
 # 筛选特定标签数据
-print(data[data['类型标签-1'] == '动画'])
-print(data[data['类型标签-1'].str.contains('科幻')])
+# print(data[data['类型标签-1'] == '动画'])
+# print(data[data['类型标签-1'].str.contains('科幻')])
 
+# 改变列表顺序
+# print(data)
+order = ['排名','电影名称','类型标签-1','类型标签','拍摄时间','全球票房(亿美元)','简介']
+data_order = data[order]
+# print(data_order)
 
 # 写入器保存数据
 with pd.ExcelWriter('temp_1.xlsx') as writer:  
